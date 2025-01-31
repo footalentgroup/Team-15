f4c299aefe9c7347a67b0eb68effe72ec8f86551
JhKsCy <merendita@gmail.com>
2025-01-31 18:15:13 -0500

    style: :lipstick: Monitoring overflow fixed

--- frontend/src/app/students/layout.tsx
+++ frontend/src/app/students/layout.tsx
@@ -20,7 +20,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className='bg-[#FFFAEB] min-h-screen flex'>
             <Sidebar currentCourse={currentCourse ? currentCourse : undefined} isVisible={isVisible} setIsVisible={setIsVisible} isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
-            <div className='h-screen w-full ms-8'>
+            <div className='h-screen w-full overflow-y-auto ms-8'>
                 {children}
             </div>
         </div>
